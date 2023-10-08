f5158d18f5f76871974140638a99d93646b919a6
Bryan Arriaga <75543058+bryanarriaga67@users.noreply.github.com>
2023-10-08 13:51:58 -0600

    Adding A*

--- PA1.py
+++ PA1.py
@@ -6,6 +6,7 @@ Bryan Arriaga
 
 import sys
 import time
+import heapq
 from collections import deque
 
 #Node class
@@ -14,6 +15,9 @@ class Node:
         self.coordinate = coordinate
         self.cost = cost
         self.parent_node = parent_node
+    def __lt__(self, other):
+        # Define custom comparison for the priority queue
+        return self.cost < other.cost
     #prints info of 
     def __str__(self):
         parent_node_coordinate = None
@@ -188,6 +192,70 @@ def depth_limited_search(starting_node, goal_coordinate, map_representation, dim
     print("Path: NULL")
     return None
 
+def A_star_search(starting_node, goal_coordinate, map_representation, dimension, cutoff_time):
+    # Keep track of visited states
+    explored = set()
+    # Priority queue (min heap) to store nodes
+    open_list = [starting_node]
+    heapq.heapify(open_list)
+    # List to keep track of max number of nodes
+    max_num_nodes = []
+    start_time = time.time()
+    current_depth = 0
+
+    while open_list:
+        # Store the number of nodes in memory
+        max_num_nodes.append(len(open_list))
+        # Popping the node with the lowest f value from the priority queue
+        current_node = heapq.heappop(open_list)
+        current_depth = current_node.cost  # Depth is equivalent to the cost of the node
+
+        # Check if the elapsed time exceeds the cutoff
+        current_time = time.time()
+        elapsed_time_ms = (current_time - start_time) * 1000
+        if elapsed_time_ms > float(cutoff_time) * 1000:
+            print("Goal Node not found within the cutoff time.")
+            return None  # Terminate the search if the cutoff time is exceeded
+
+        # Add current node to explored set
+        explored.add(tuple(current_node.coordinate))
+
+        if current_node.coordinate == goal_coordinate:
+            # Goal node found, reconstruct the path
+            path = [current_node.coordinate]
+            path_cost = current_node.cost
+            while current_node.parent_node is not None:
+                current_node = current_node.parent_node
+                path.append(current_node.coordinate)
+                path_cost += current_node.cost
+            path.reverse()  # Reverse the path to get it in the correct order
+
+            end_time = time.time()  # Record the end time
+            runtime_ms = (end_time - start_time) * 1000
+            print("1) Cost of path:", path_cost)
+            print("2) Number of nodes expanded:", len(explored))
+            print("3) Maximum number of nodes in memory:", max(max_num_nodes))
+            print("4) Runtime of algorithm:", runtime_ms, "milliseconds")
+            print("5) Path:", path)
+            return path
+
+        
+        for successor_node in generate_successor_nodes(current_node, map_representation, dimension):
+            # Check for repeated states
+            if tuple(successor_node.coordinate) not in explored:
+                heapq.heappush(open_list, successor_node)  # Push successor nodes onto the priority queue
+
+    # If the loop completes without finding the goal, no path exists
+    end_time = time.time()  # Record the end time
+    runtime_ms = (end_time - start_time) * 1000
+    print("Goal Node not found")
+    print("Cost of Path: -1")
+    print("Number of nodes expanded:", len(explored))
+    print("Maximum number of nodes in memory:", max(max_num_nodes))
+    print("Runtime of algorithm:", runtime_ms, "Milliseconds")
+    print("Path: NULL")
+    return None
+
 
 if len(sys.argv) != 4:
     print("Usage: python3 PA1.py <filename> <search algorithm> <cutoff time in seconds>")
@@ -224,5 +292,6 @@ if search_algorithm == 'IDS':
     depth_limit = int(sys.argv[4])
     depth_limited_search(starting_node, goal_coordinate, map_representation, dimension, cutoff_time,depth_limit)
         
-    
+if search_algorithm == 'AS':
+    A_star_search(starting_node, goal_coordinate, map_representation, dimension,cutoff_time)   
 
